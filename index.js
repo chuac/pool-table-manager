65714016dca0f9a1e487e78ecb9cbec3ca7cd395
Christopher Chua <chuachristopher@hotmail.com>
2020-09-19 09:14:08 +0800

    Other parsers

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const SerialPort = require('serialport');
+const ByteLength = require('@serialport/parser-byte-length')
 const Readline = require('@serialport/parser-readline');
 
 // define the serial port
 const port = new SerialPort('COM4', { // check device manager for COM number
     baudRate: 9600
 });
-
+console.log('Program listening');
 
 // read data from serial port normally
-// port.on('data', (data) => console.log('Data:', data));
+port.on('data', (data) => console.log('Data:', data.toString()));
 
 // serial port parser
-const parser = port.pipe(new Readline());
-
+// const parser = port.pipe(new Readline());
 // read data from serial port, using the parser
-parser.on('data', (line) => console.log(line));
+// parser.on('data', (data) => console.log(data));
+
+// const parser = port.pipe(new ByteLength({length: 4}))
+// parser.on('data', (data) => console.log(data.toString()));
