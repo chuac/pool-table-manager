d2e78c15e4cb8ef4da811f8ff85a9536773dfb5c
Jens <jensadria@gmail.com>
2021-11-28 11:16:04 +0800

    Test

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // import { Buffer } from 'buffer';
 const Buffer = require('buffer').Buffer;
 const SerialPort = require('serialport');
-const ByteLength = require('@serialport/parser-byte-length')
+const ByteLength = require('@serialport/parser-byte-length');
 const Readline = require('@serialport/parser-readline');
 
 // define the serial port
+<<<<<<< HEAD
 const port = new SerialPort('COM7', { // check device manager for COM number
     baudRate: 9600
+=======
+const port = new SerialPort('COM1', {
+  // check device manager for COM number
+  baudRate: 9600,
+>>>>>>> 63b4fda (Test)
 });
 console.log('Program listening');
 
@@ -20,6 +26,7 @@ port.on('data', (data) => console.log('Data:', Buffer.from(data, 'hex').toString
 // read data from serial port, using the parser
 // parser.on('data', (data) => console.log(data));
 
+<<<<<<< HEAD
 // const parser = port.pipe(new ByteLength({length: 8}))
 // parser.on('data', (data) => console.log(data.toString()));
 // export enum CoolEnumName {
@@ -29,3 +36,7 @@ port.on('data', (data) => console.log('Data:', Buffer.from(data, 'hex').toString
 // }
 
 // console.log(CoolEnumName.ONE_ON);
+=======
+// const parser = port.pipe(new ByteLength({length: 4}))
+// parser.on('data', (data) => console.log(data.toString()));
+>>>>>>> 63b4fda (Test)
