820b6c1788f61a758b82152f88c0adf336120716
chuac <chuachristopher@hotmail.com>
2021-11-28 11:14:49 +0800

    experiment (#1)

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+// import { Buffer } from 'buffer';
+const Buffer = require('buffer').Buffer;
 const SerialPort = require('serialport');
 const ByteLength = require('@serialport/parser-byte-length')
 const Readline = require('@serialport/parser-readline');
 
 // define the serial port
-const port = new SerialPort('COM4', { // check device manager for COM number
+const port = new SerialPort('COM7', { // check device manager for COM number
     baudRate: 9600
 });
 console.log('Program listening');
 
 // read data from serial port normally
-port.on('data', (data) => console.log('Data:', data.toString()));
+port.on('data', (data) => console.log('Data:', Buffer.from(data, 'hex').toString('hex')));
+// port.on('data', (data) => console.log('Data:', data));
+// port.on('data', (data) => console.log('Data:', Buffer.from(data, 'hex').toString('utf8')));
 
 // serial port parser
 // const parser = port.pipe(new Readline());
 // read data from serial port, using the parser
 // parser.on('data', (data) => console.log(data));
 
-// const parser = port.pipe(new ByteLength({length: 4}))
+// const parser = port.pipe(new ByteLength({length: 8}))
 // parser.on('data', (data) => console.log(data.toString()));
+// export enum CoolEnumName {
+
+//     ONE_ON = 41,
+//     ONE_OFF = 01,
+// }
+
+// console.log(CoolEnumName.ONE_ON);
