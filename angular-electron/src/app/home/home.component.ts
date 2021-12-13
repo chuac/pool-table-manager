a2817006aa7a5ae521281405e344c99ed93a7c3e
Christopher Chua <chuachristopher@hotmail.com>
2021-12-13 22:27:30 +0800

    Open and listen to a COM port, added types for SerialPort

--- angular-electron/src/app/home/home.component.ts
+++ angular-electron/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { ElectronService } from './../core/services/electron/electron.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-// import { SerialPortService } from '../serial-port.service';
+import { SerialPort } from 'electron';
+import { PortInfo } from 'serialport';
 
 @Component({
   selector: 'app-home',
@@ -20,8 +21,25 @@ export class HomeComponent implements OnInit {
     this.electronService.serialPort.list().then(ports => {
       ports.forEach(port => {
         console.log(port);
-      })
-    })
+      });
+
+      const desiredPort = ports.filter((port) => {
+        return port.locationId && port.serialNumber;
+      });
+
+      if (desiredPort.length === 1) {
+        const port = new this.electronService.serialPort(
+          desiredPort[0].path, { baudRate: 9600 }
+        );
+
+        port.on('data', (data) =>
+          console.log('Data:', Buffer.from(data, 'hex').toString('hex'))
+        );
+      } else {
+        console.log('MORE THAN ONE "ELIGIBLE" COM PORT WAS FOUND');
+      }
+    });
+
 
   }
 
