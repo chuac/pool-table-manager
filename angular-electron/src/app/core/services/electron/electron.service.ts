36f3d7b39d53714d551dcb783954dd8cb4442ffc
Christopher Chua <chuachristopher@hotmail.com>
2021-12-18 11:55:56 +0800

    Setup SerialPortService with RxJS

--- angular-electron/src/app/core/services/electron/electron.service.ts
+++ angular-electron/src/app/core/services/electron/electron.service.ts
@@ -8,40 +8,62 @@ import * as fs from 'fs';
 import * as SerialPort from 'serialport';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ElectronService {
-  ipcRenderer: typeof ipcRenderer;
-  webFrame: typeof webFrame;
-  childProcess: typeof childProcess;
-  fs: typeof fs;
-  serialPort: typeof SerialPort;
+	ipcRenderer: typeof ipcRenderer;
+	webFrame: typeof webFrame;
+	childProcess: typeof childProcess;
+	fs: typeof fs;
+	serialPort: typeof SerialPort;
 
-  constructor() {
-    // Conditional imports
-    if (this.isElectron) {
-      this.ipcRenderer = window.require('electron').ipcRenderer;
-      this.webFrame = window.require('electron').webFrame;
+	constructor() {
+		// Conditional imports
+		if (this.isElectron) {
+			this.ipcRenderer = window.require('electron').ipcRenderer;
+			this.webFrame = window.require('electron').webFrame;
 
-      this.childProcess = window.require('child_process');
-      this.fs = window.require('fs');
-      this.serialPort = window.require('serialport');
+			this.childProcess = window.require('child_process');
+			this.fs = window.require('fs');
+			this.serialPort = window.require('serialport');
 
-      // Notes :
-      // * A NodeJS's dependency imported with 'window.require' MUST BE present in `dependencies` of both `app/package.json`
-      // and `package.json (root folder)` in order to make it work here in Electron's Renderer process (src folder)
-      // because it will loaded at runtime by Electron.
-      // * A NodeJS's dependency imported with TS module import (ex: import { Dropbox } from 'dropbox') CAN only be present
-      // in `dependencies` of `package.json (root folder)` because it is loaded during build phase and does not need to be
-      // in the final bundle. Reminder : only if not used in Electron's Main process (app folder)
+			this.serialPort.list().then(ports => {
+				ports.forEach(port => {
+					console.log(port); // TODO: Remove
+				});
 
-      // If you want to use a NodeJS 3rd party deps in Renderer process,
-      // ipcRenderer.invoke can serve many common use cases.
-      // https://www.electronjs.org/docs/latest/api/ipc-renderer#ipcrendererinvokechannel-args
-    }
-  }
+				const desiredPort = ports.filter((port) => {
+					return port.locationId && port.serialNumber; // TODO: Investigate a more robust way
+				});
 
-  get isElectron(): boolean {
-    return !!(window && window.process && window.process.type);
-  }
+				if (desiredPort.length === 1) {
+					const port = new this.serialPort(
+						desiredPort[0].path, { baudRate: 9600 }
+					);
+
+					port.on('data', (data) =>
+						console.log('Data:', Buffer.from(data, 'hex').toString('hex'))
+					);
+				} else {
+					console.log('MORE THAN ONE "ELIGIBLE" COM PORT WAS FOUND');
+				}
+			});
+
+			// Notes :
+			// * A NodeJS's dependency imported with 'window.require' MUST BE present in `dependencies` of both `app/package.json`
+			// and `package.json (root folder)` in order to make it work here in Electron's Renderer process (src folder)
+			// because it will loaded at runtime by Electron.
+			// * A NodeJS's dependency imported with TS module import (ex: import { Dropbox } from 'dropbox') CAN only be present
+			// in `dependencies` of `package.json (root folder)` because it is loaded during build phase and does not need to be
+			// in the final bundle. Reminder : only if not used in Electron's Main process (app folder)
+
+			// If you want to use a NodeJS 3rd party deps in Renderer process,
+			// ipcRenderer.invoke can serve many common use cases.
+			// https://www.electronjs.org/docs/latest/api/ipc-renderer#ipcrendererinvokechannel-args
+		}
+	}
+
+	get isElectron(): boolean {
+		return !!(window && window.process && window.process.type);
+	}
 }
--- angular-electron/src/app/home/home.component.ts
+++ angular-electron/src/app/home/home.component.ts
@@ -18,27 +18,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
-    this.electronService.serialPort.list().then(ports => {
-      ports.forEach(port => {
-        console.log(port);
-      });
 
-      const desiredPort = ports.filter((port) => {
-        return port.locationId && port.serialNumber;
-      });
-
-      if (desiredPort.length === 1) {
-        const port = new this.electronService.serialPort(
-          desiredPort[0].path, { baudRate: 9600 }
-        );
-
-        port.on('data', (data) =>
-          console.log('Data:', Buffer.from(data, 'hex').toString('hex'))
-        );
-      } else {
-        console.log('MORE THAN ONE "ELIGIBLE" COM PORT WAS FOUND');
-      }
-    });
 
 
   }
