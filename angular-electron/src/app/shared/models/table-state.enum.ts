e828e3dab996f52a1f5965e6d8a6f9542d2edde5
Jens <jensadria@gmail.com>
2022-01-05 21:12:31 +0800

    Merge branch 'master' of https://github.com/chuac/pool-table-manager

--- angular-electron/src/app/shared/models/table-state.enum.ts
+++ angular-electron/src/app/shared/models/table-state.enum.ts
@@ -3,4 +3,5 @@ export enum TableState {
 	On = 'On',
 	Off = 'Off',
 	Clean = 'Clean',
+    Disabled = 'Disabled'  // Working On Design Now, the Tabels that are disabled will be blacked out on the screen
 }
