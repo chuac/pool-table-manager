f553b0223d1f2cce157058848a21fd08f10172df
Christopher Chua <chuachristopher@hotmail.com>
2022-01-26 16:59:00 +0800

    install angular material for dialogs

--- angular-electron/src/app/app.module.ts
+++ angular-electron/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { DateFnsConfigurationService } from 'ngx-date-fns';
@@ -11,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { HomeModule } from './home/home.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { UserInputModule } from './user-input/user-input.module';
 
 import { AppComponent } from './app.component';
 
@@ -24,9 +26,11 @@ australianDateConfig.setLocale(enAU);
 		FormsModule,
 		HttpClientModule,
 		CoreModule,
+		BrowserAnimationsModule,
 		SharedModule,
 		HomeModule,
 		TransactionModule,
+		UserInputModule,
 		AppRoutingModule,
 	],
 	providers: [
